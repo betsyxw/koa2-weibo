9461fd44269fe0f3e59087ffb282f0362e73cf07
betsywenwen <betsywenwen@yahoo.co.jp>
2021-06-13 18:36:31 +0800

    feat:router

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,14 +6,32 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   }
 })
+
+//博客个人主页
+router.get('/profile/:userName', async(ctx,next)=>{
+  const {userName } = ctx.params
+  ctx.body ={
+    title: 'this is =>profile page',
+    userName
+  }
+})
+
+//加载更多
+//get请求，直接从ctx.params中获取参数
+router.get('/loadMore/:userName/:pageIndex', async(ctx,next)=>{
+  const {userName,pageIndex} = ctx.params
+  ctx.body = {
+    title:'this is loading page API',
+    userName,
+    pageIndex
+  }
+})
+
+
 
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -15,4 +15,15 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+//Post
+//function(ctx,next){} ===async(ctx,next)=>{}
+router.post('/login',async(ctx,next)=>{
+  //传入用户名+密码,data在ctx.request.body里面
+  const {userName, password} = ctx.request.body
+  ctx.body = {
+    userName,
+    password
+  }
+} )
+
 module.exports = router
